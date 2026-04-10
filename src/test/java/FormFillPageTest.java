0e4dfebcb7e7a2be774eb2a8817e3a0c38fdd072
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-04-10 21:57:36 +0530

    Add new Project File

---
+++ src/test/java/FormFillPageTest.java
@@ -0,0 +1,45 @@
+import com.applicationFormPages.FormFillPage;
+import com.applicationFormPages.SelectFormPage;
+import com.basePage.BaseClass;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FormFillPageTest
+{
+    public BaseClass baseClass;
+    public SelectFormPage selectFormPage;
+    public FormFillPage formFillPage;
+
+    @BeforeMethod
+    public void setUp()
+    {
+        baseClass=new BaseClass();
+        baseClass.navigateToApplication();
+
+        selectFormPage=new SelectFormPage();
+        selectFormPage.navigateToPracticeForm();
+        Assert.assertTrue(selectFormPage.verifyMessage.isDisplayed(),
+                "Couldn't navigate to Practice Form");
+    }
+
+    @Test
+    public void verifyFormFill()
+    {
+        formFillPage=new FormFillPage();
+        formFillPage.formFill(baseClass.properties.getProperty("FirstName"),
+                baseClass.properties.getProperty("LastName"), baseClass.properties.getProperty("Email"),
+                baseClass.properties.getProperty("MobileNumber"),
+                baseClass.properties.getProperty("DateOfBirth"), baseClass.properties.getProperty("Subject"),
+                baseClass.properties.getProperty("Picture"), baseClass.properties.getProperty("Address"),
+                baseClass.properties.getProperty("State"), baseClass.properties.getProperty("City"));
+        Assert.assertTrue(formFillPage.verifyMessage.isDisplayed(),"Application not saved");
+    }
+
+    @AfterMethod
+    public void tearDown()
+    {
+        baseClass.quitApplications();
+    }
+}
