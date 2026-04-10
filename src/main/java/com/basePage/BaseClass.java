85a7ed3542b5d679d5615a7dafbb4c8269c8fa74
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-04-10 21:53:14 +0530

    Add new Project File

---
+++ src/main/java/com/basePage/BaseClass.java
@@ -0,0 +1,68 @@
+package com.basePage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class BaseClass
+{
+    public static WebDriver driver;
+    public Properties properties;
+    public static WebDriverWait wait;
+
+    public BaseClass()
+    {
+        String filePath="C://Users//ELCOT//IdeaProjects//StudentApplicationForm//src//main//resources//InputData";
+        properties=new Properties();
+        try
+        {
+            FileInputStream file=new FileInputStream(filePath);
+            properties.load(file);
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public void navigateToApplication()
+    {
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        wait=new WebDriverWait(driver,Duration.ofSeconds(40));
+        driver.get(properties.getProperty("url"));
+    }
+
+    public void writeText(String text, WebElement element)
+    {
+        wait.until(ExpectedConditions.visibilityOf(element)).sendKeys(text);
+    }
+
+    public void clickButton(WebElement element)
+    {
+        wait.until(ExpectedConditions.visibilityOf(element)).click();
+    }
+
+    public void quitApplications()
+    {
+        try
+        {
+            Thread.sleep(10000);
+            driver.quit();
+        }
+        catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+}
