04eaf8d3cd827b98be7dd7cdbf6d7d8287b50a99
BalajiPerumalsamy <pbalaji23082001@gmail.com>
2026-04-10 21:57:19 +0530

    Add new Project File

---
+++ src/test/java/SelectFormPageTest.java
@@ -0,0 +1,33 @@
+import com.applicationFormPages.SelectFormPage;
+import com.basePage.BaseClass;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class SelectFormPageTest
+{
+    public BaseClass baseClass;
+    public SelectFormPage selectFormPage;
+
+    @BeforeMethod
+    public void setUp()
+    {
+      baseClass=new BaseClass();
+      baseClass.navigateToApplication();
+    }
+
+    @Test
+    public void verifyPracticeForm()
+    {
+        selectFormPage=new SelectFormPage();
+        selectFormPage.navigateToPracticeForm();
+        Assert.assertTrue(selectFormPage.verifyMessage.isDisplayed(),"Couldn't navigate to Practice Form");
+    }
+
+    @AfterMethod
+    public void tearDown()
+    {
+        baseClass.quitApplications();
+    }
+}
